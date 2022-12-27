7e492ea68aff538e01a1fbc01ba3a469d82a2ec1
Vitaly Reutsky <vreutsky77@mail.ru>
2022-12-27 14:21:55 +0300

    Адаптировал модальные окна под планшеты, убрал клик на выпадающее меню и изменил отступ в секции социальных иконок

--- src/js/components/catalog-menu.js
+++ src/js/components/catalog-menu.js
@@ -1,63 +1,63 @@
-//работа с menu
-const catalogMenu = document.querySelector(".menu");
-const catalogMenuItems = document.querySelectorAll(".menu__item");
+////работа с menu
+//const catalogMenu = document.querySelector(".menu");
+//const catalogMenuItems = document.querySelectorAll(".menu__item");
 
-catalogMenuItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.style.cursor = "grab";
-    item.querySelector(".menu__link").style.cursor = "grab";
-  });
-});
+//catalogMenuItems.forEach((item) => {
+//  item.addEventListener("click", () => {
+//    item.style.cursor = "grab";
+//    item.querySelector(".menu__link").style.cursor = "grab";
+//  });
+//});
 
-if (catalogMenu) {
-  document.addEventListener("click", (e) => {
-    const catalogMenu = document.querySelector(".menu__list");
-    const catalogMenuLink = catalogMenu.querySelectorAll(".menu__link");
+//if (catalogMenu) {
+//  document.addEventListener("click", (e) => {
+//    const catalogMenu = document.querySelector(".menu__list");
+//    const catalogMenuLink = catalogMenu.querySelectorAll(".menu__link");
 
-    catalogMenuLink.forEach((link) => {
-      const subMenu = link.nextElementSibling;
+//    catalogMenuLink.forEach((link) => {
+//      const subMenu = link.nextElementSibling;
 
-      if (subMenu.classList.contains("sub-menu--active")) {
-        const subMenuItems = subMenu.querySelectorAll(".sub-menu__item");
-        subMenuItems.forEach((subMenuItem) => {
-          if (
-            e.target !== link &&
-            e.target !== link.parentElement &&
-            e.target !== catalogMenu &&
-            e.target !== catalogMenu.parentElement &&
-            e.target !== link.nextElementSibling &&
-            e.target !== subMenu.querySelector(".sub-menu__list") &&
-            e.target !== subMenuItem &&
-            e.target !== subMenuItem.querySelector(".sub-menu__link")
-          ) {
-            if (link.classList.contains("menu-link--active")) {
-              link.classList.remove("menu-link--active");
-              link.nextElementSibling.classList.remove("sub-menu--active");
-            }
-          }
-        });
-      }
-    });
-  });
+//      if (subMenu.classList.contains("sub-menu--active")) {
+//        const subMenuItems = subMenu.querySelectorAll(".sub-menu__item");
+//        subMenuItems.forEach((subMenuItem) => {
+//          if (
+//            e.target !== link &&
+//            e.target !== link.parentElement &&
+//            e.target !== catalogMenu &&
+//            e.target !== catalogMenu.parentElement &&
+//            e.target !== link.nextElementSibling &&
+//            e.target !== subMenu.querySelector(".sub-menu__list") &&
+//            e.target !== subMenuItem &&
+//            e.target !== subMenuItem.querySelector(".sub-menu__link")
+//          ) {
+//            if (link.classList.contains("menu-link--active")) {
+//              link.classList.remove("menu-link--active");
+//              link.nextElementSibling.classList.remove("sub-menu--active");
+//            }
+//          }
+//        });
+//      }
+//    });
+//  });
 
-  catalogMenuItems.forEach((item) => {
-    const subMenu = item.querySelector(".sub-menu");
-    const menuLink = item.querySelector(".menu__link");
-    if (subMenu.classList.contains("sub-menu--active")) {
-      subMenu.classList.remove("sub-menu--active");
-      menuLink.classList.remove("menu-link--active");
-    }
+//  catalogMenuItems.forEach((item) => {
+//    const subMenu = item.querySelector(".sub-menu");
+//    const menuLink = item.querySelector(".menu__link");
+//    if (subMenu.classList.contains("sub-menu--active")) {
+//      subMenu.classList.remove("sub-menu--active");
+//      menuLink.classList.remove("menu-link--active");
+//    }
 
-    item.addEventListener("click", (e) => {
-      document
-        .querySelector(".menu-link--active")
-        ?.classList?.remove("menu-link--active");
-      menuLink.classList.toggle("menu-link--active");
+//    item.addEventListener("click", (e) => {
+//      document
+//        .querySelector(".menu-link--active")
+//        ?.classList?.remove("menu-link--active");
+//      menuLink.classList.toggle("menu-link--active");
 
-      document
-        .querySelector(".sub-menu--active")
-        ?.classList?.remove("sub-menu--active");
-      subMenu.classList.toggle("sub-menu--active");
-    });
-  });
-}
+//      document
+//        .querySelector(".sub-menu--active")
+//        ?.classList?.remove("sub-menu--active");
+//      subMenu.classList.toggle("sub-menu--active");
+//    });
+//  });
+//}
